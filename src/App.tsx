a07a1e1f560476d2121a572bcfd27febd1d2bb1a
prannav225 <nagapranav.m@gmail.com>
2025-03-05 13:34:52 +0530

    add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import './index.css'
-import Header from './components/Header';
-import About from './components/About';
 import Footer from './components/Footer';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <div>
-      <Header />
-      <About />
+      <Home/>
       <Footer />
     </div>
   );
---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import Header from '../components/Header';
+import About from '../components/About';
+import Works from '../components/Works';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <About />
+      <Works/>
+    </div>
+  )
+}
+
+export default Home;
