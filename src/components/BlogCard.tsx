d10903d210b38140b37f82851f0db0c75a760541
prannav225 <nagapranav.m@gmail.com>
2025-03-05 16:24:36 +0530

    add blog

---
+++ src/components/BlogCard.tsx
@@ -0,0 +1,34 @@
+interface BlogCardProps {
+    date: string;
+    title: string;
+    excerpt: string;
+    imageUrl: string;
+    slug: string;
+}
+
+const BlogCard = ({ date, title, excerpt, imageUrl, slug }: BlogCardProps) => {
+    return (
+        <div className="flex mb-16 justify-center items-center py-10 gap-9">
+            <div className="">
+                <img 
+                    src={imageUrl} 
+                    alt={title}
+                    className="rounded-xl object-cover"
+                />
+            </div>
+            <div className="flex flex-col justify-center">
+                <p className="text-gray-500 mb-4">{date}</p>
+                <h2 className="text-3xl font-family mb-4">{title}</h2>
+                <p className="text-gray-700 mb-6">{excerpt}</p>
+                <a 
+                    href={`/blog/${slug}`} 
+                    className="text-[--color-blue] hover:opacity-80 transition duration-300 inline-flex items-center pt-3 text-sm font-semibold underline underline-offset-4"
+                >
+                    CONTINUE READING →
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default BlogCard;
---
+++ src/pages/Blog.tsx
@@ -0,0 +1,42 @@
+import BlogCard from '../components/BlogCard'
+import Header from '../components/Header'
+
+const Blog = () => {
+    return (
+        <div>
+            <Header />
+            <div className='bg-gradient-to-r from-[--color-darker] to-[--color-dark] text-white grid grid-cols-2 gap-6 px-52 py-10 mb-20'>
+                <div className='flex flex-col justify-center'>
+                    <h1 className='text-6xl font-family uppercase font-semibold tracking-wide'>Blog</h1>
+                    <p className='text-lg pr-10'>My personal blog focused on “building businesses online” where I share my journey and experiences with you.</p>
+                </div>
+                <div>
+                    <img src="https://placehold.co/600x400" alt="" className='rounded-tr-[60px] rounded-bl-[60px] rounded-md' />
+                </div>
+            </div>
+            <BlogCard
+                date="JANUARY 1, 2025"
+                title="This Happened To Me In 2024 – Year In Review"
+                excerpt="2024 Has Come To An End, And I Am Grateful For All The New Opportunities As Well As..."
+                imageUrl="https://placehold.co/400x300"
+                slug="2024-year-in-review"
+            />
+            <BlogCard
+                date="JANUARY 1, 2025"
+                title="This Happened To Me In 2024 – Year In Review"
+                excerpt="2024 Has Come To An End, And I Am Grateful For All The New Opportunities As Well As..."
+                imageUrl="https://placehold.co/400x300"
+                slug="2024-year-in-review"
+            />
+            <BlogCard
+                date="JANUARY 1, 2025"
+                title="This Happened To Me In 2024 – Year In Review"
+                excerpt="2024 Has Come To An End, And I Am Grateful For All The New Opportunities As Well As..."
+                imageUrl="https://placehold.co/400x300"
+                slug="2024-year-in-review"
+            />
+        </div>
+    )
+}
+
+export default Blog
