5a5be4ed60942f825c70334328c0f7b4f585b5e3
prannav225 <nagapranav.m@gmail.com>
2025-03-05 16:22:11 +0530

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import './index.css'
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import About from './pages/About';
+import './styles/colors.css'; 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Blog from './pages/Blog';
+import Gallery from './pages/Gallery';
+import Contact from './pages/Contact';
 
 const App = () => {
   return (
-    <div>
-      <Home/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 };
 
