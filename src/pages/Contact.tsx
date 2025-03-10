880e83f60beb7cb60c0a5500a105fa615f43aefb
prannav225 <nagapranav.m@gmail.com>
2025-03-10 17:15:16 +0530

    add location

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -15,8 +15,8 @@ const Contact = () => {
                         <form className='space-y-4 md:space-y-6'>
                             <div>
                                 <label htmlFor="name" className='block mb-2'>Name</label>
-                                <input 
-                                    type="text" 
+                                <input
+                                    type="text"
                                     id="name"
                                     placeholder="Rachel Joe"
                                     className='w-full p-3 md:p-4 rounded-md outline-none border border-[--color-blue]'
@@ -24,23 +24,23 @@ const Contact = () => {
                             </div>
                             <div>
                                 <label htmlFor="email" className='block mb-2'>Email</label>
-                                <input 
-                                    type="email" 
+                                <input
+                                    type="email"
                                     id="email"
                                     placeholder="Rachel@domain.com"
-                                   className='w-full p-3 md:p-4 rounded-md outline-none border border-[--color-blue]'
+                                    className='w-full p-3 md:p-4 rounded-md outline-none border border-[--color-blue]'
                                 />
                             </div>
                             <div>
                                 <label htmlFor="message" className='block mb-2'>Message</label>
-                                <textarea 
+                                <textarea
                                     id="message"
                                     rows={6}
                                     placeholder="Type your message here..."
                                     className='w-full p-3 md:p-4 rounded-md outline-none border border-[--color-blue]'
                                 />
                             </div>
-                            <button 
+                            <button
                                 type="submit"
                                 className='bg-[--color-blue] text-white px-6 md:px-8 py-3 md:py-4 rounded-md hover:bg-[--color-dark] transition duration-300'
                             >
@@ -50,14 +50,13 @@ const Contact = () => {
                     </div>
 
                     {/* Map */}
-                    <div className='h-[300px] sm:h-[400px] md:h-[500px] lg:h-[700px] w-full lg:w-[450px] static lg:absolute lg:top-[-100px] lg:right-0 mt-8 lg:mt-0'>
-                        <iframe 
-                            src="https://www.google.com/maps/embed?pb=YOUR_MAP_EMBED_URL"
-                            width="100%" 
-                            height="100%" 
-                            style={{ border: 0 }} 
-                            allowFullScreen 
-                            loading="lazy" 
+                    <div className='h-[300px] sm:h-[400px] md:h-[500px] lg:h-[700px] w-full lg:w-[450px] static lg:absolute lg:top-[-200px] lg:right-0 mt-8 lg:mt-0'>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d124432.021170122!2d77.578006!3d12.939783!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1593946f9db7%3A0x315774e67ca32921!2sal-ameen%20apartments!5e0!3m2!1sen!2sus!4v1741601787405!5m2!1sen!2sus"
+                            width="100%"
+                            height="100%"
+                            style={{ border: 0 }}
+                            allowFullScreen
+                            loading="lazy"
                             referrerPolicy="no-referrer-when-downgrade"
                             className='rounded-lg shadow-2xl'
                         />
